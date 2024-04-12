643cc0aaa3a6ee9e9617277d5c4fb04bb617c454
Adarsh Kamath <adarshkmt2004@gmail.com>
2024-04-12 15:24:13 +0530

    updated main package to reflect changes in application structure, main method initializes database, repository, service, controller, http router objects, handles dependency injection.

--- main.go
+++ main.go
@@ -3,25 +3,38 @@ package main
 import (
 	"context"
 	"log"
-	//"github.com/gorilla/websocket"
-	// "github.com/joho/godotenv"
+	"net/http"
+
+	"github.com/Adarsh-Kmt/chatapp/controller"
+	"github.com/Adarsh-Kmt/chatapp/database"
+	"github.com/Adarsh-Kmt/chatapp/repository"
+	"github.com/Adarsh-Kmt/chatapp/service"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
-	storage, err := NewMongoDBInstance()
+	storage, err := database.NewMongoDBInstance()
 
 	if err != nil {
 
 		log.Fatal(err.Error())
 	}
 
+	userRepository := repository.NewUserRepositoryImplInstance(storage)
+
+	userService := service.NewUserServiceImplInstance(userRepository)
+
+	userController := controller.NewUserControllerInstance(userService)
+	MainRouter := mux.NewRouter()
+	userController.Run(MainRouter)
+
+	http.ListenAndServe(":3000", MainRouter)
+
 	defer func() {
-		if err := storage.db.Disconnect(context.TODO()); err != nil {
+		if err := storage.DB.Disconnect(context.TODO()); err != nil {
 			panic(err)
 		}
 	}()
-	APIServer := NewAPIServer(":3000", storage)
 
-	APIServer.Run()
 }
