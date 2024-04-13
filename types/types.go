77e1d2e4d59cf2bcce35175ee1538c52d6dcb673
Adarsh Kamath <adarshkmt2004@gmail.com>
2024-04-13 13:18:52 +0530

    added incoming/outgoing chat request list response structs.

--- types/types.go
+++ types/types.go
@@ -27,19 +27,19 @@ type LoginUserRequest struct {
 }
 
 type Chat struct {
-	ChatId      primitive.ObjectID `json:"chatId" bson:"_id, omitempty"`
+	ChatId      primitive.ObjectID `json:"chatId" bson:"_id,omitempty"`
 	User1       primitive.ObjectID `json:"user1ObjectId" bson:"User1ObjectId"`
 	User2       primitive.ObjectID `json:"user2ObjectId" bson:"User2ObjectId"`
 	MessageList []Message          `json:"messageList" bson:"MessageList"`
 }
 
 type ChatSummary struct {
-	ChatSummaryId   primitive.ObjectID `json:"chatSummaryId" bson:"_id, omitempty"`
+	ChatSummaryId   primitive.ObjectID `json:"chatSummaryId" bson:"_id,omitempty"`
 	User            primitive.ObjectID `json:"userObjectId" bson:"UserObjectId"`
 	ChatReferenceId primitive.ObjectID `json:"chatReferenceId" bson:"ChatReferenceId"`
 }
 type Message struct {
-	MessageId  primitive.ObjectID `json:"messageId" bson:"_id, omitempty"`
+	MessageId  primitive.ObjectID `json:"messageId" bson:"_id,omitempty"`
 	SenderId   primitive.ObjectID `json:"senderId" bson:"SenderObjectId"`
 	ReceiverId primitive.ObjectID `json:"receiverId" bson:"ReceiverObjectId"`
 	Message    string             `json:"message" bson:"Message"`
@@ -47,7 +47,7 @@ type Message struct {
 }
 
 type ChatRequest struct {
-	ChatRequestId   primitive.ObjectID `json:"chatRequestId" bson:"_id, omitempty"`
+	ChatRequestId   primitive.ObjectID `json:"chatRequestId" bson:"_id,omitempty"`
 	SenderId        primitive.ObjectID `json:"senderId" bson:"SenderObjectId"`
 	ReceiverId      primitive.ObjectID `json:"receiverId" bson:"ReceiverObjectId"`
 	SentOnDate      time.Time          `json:"sentOnDate" bson:"SentOnDate"`
@@ -62,4 +62,12 @@ type MessageRequest struct {
 type APIError struct {
 	Error       string
 	ErrorStatus int
+}
+
+type OutgoingChatRequestListResponse struct {
+	OutgoingChatRequestList []ChatRequest `json:"outgoingChatRequestList" bson:"OutgoingChatRequestList"`
+}
+
+type IncomingChatRequestListResponse struct {
+	IncomingChatRequestList []ChatRequest `json:"incomingChatRequestList" bson:"IncomingChatRequestList"`
 }
