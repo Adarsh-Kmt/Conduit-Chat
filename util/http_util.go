89d31ec93ae94624a9f701fb6326cee17112f712
Adarsh Kamath <adarshkmt2004@gmail.com>
2024-04-13 12:54:35 +0530

    created http_util file for WriteJSON method (used to write json in the http response body), makeHttpHandlerFunc method (enables application to return error responses).

---
+++ util/http_util.go
@@ -0,0 +1,28 @@
+package util
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Adarsh-Kmt/chatapp/types"
+)
+
+type ApiFunc func(http.ResponseWriter, *http.Request) *types.APIError
+
+func MakeHttpHandlerFunc(f ApiFunc) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		if APIError := f(w, r); APIError != nil {
+
+			WriteJSON(w, APIError.ErrorStatus, map[string]string{"error": APIError.Error})
+		}
+	}
+}
+
+func WriteJSON(w http.ResponseWriter, status int, body any) error {
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(body)
+}
