485df728b644bf92963ecb3d096d269abd221d6b
Adarsh Kamath <adarshkmt2004@gmail.com>
2024-06-24 17:48:06 +0530

    creatd message service, acts as dependency for user controller.

--- main.go
+++ main.go
@@ -14,25 +14,32 @@ import (
 
 func main() {
 
-	storage, err := database.NewMongoDBInstance()
+	MongoDB, err := database.NewMongoDBInstance()
 
 	if err != nil {
 
 		log.Fatal(err.Error())
 	}
 
-	userRepository := repository.NewUserRepositoryImplInstance(storage)
+	//RedisDB := database.NewRedisDBInstance()
+
+	//chatRepository := repository.NewChatRepositoryImplInstance(RedisDB)
+	userRepository := repository.NewUserRepositoryImplInstance(MongoDB)
 
 	userService := service.NewUserServiceImplInstance(userRepository)
+	messageService := service.MakeMessageServiceImplInstance(userRepository)
 
-	userController := controller.NewUserControllerInstance(userService)
+	//chatService := service.NewChatServiceImplInstance(chatRepository)
+
+	userController := controller.NewUserControllerInstance(userService, messageService)
+
 	MainRouter := mux.NewRouter()
 	userController.Run(MainRouter)
 
 	http.ListenAndServe(":3000", MainRouter)
 
 	defer func() {
-		if err := storage.DB.Disconnect(context.TODO()); err != nil {
+		if err := MongoDB.MongoDBClient.Disconnect(context.TODO()); err != nil {
 			panic(err)
 		}
 	}()
